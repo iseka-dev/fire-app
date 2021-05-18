4d0e5e6a5c0ac58d7619a4ea395bd9a25f681719
Horacio Iseka <horacio.chiarella@gmail.com>
2021-05-17 22:34:29 -0300

    requirements for new buildpack

--- fire-app-cba/wsgi.py
+++ fire-app-cba/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fire-map-cba.settings')
+os.environ["DJANGO_SETTINGS_MODULE"] = "fire-map-cba.settings"
 
 application = get_wsgi_application()
