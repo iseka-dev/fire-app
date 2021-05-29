2d83425a3ae06c41a3ef3ad0cf62efc38019d27f
Horacio Iseka <horacio.chiarella@gmail.com>
2021-05-29 17:01:19 -0300

    dj changed

--- fire-app-cba/settings.py
+++ fire-app-cba/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'fire-app-cba.wsgi.application'
 
 DATABASES = {}
 DATABASE_URL = 'postgres://mvonrtfgaovwhi:90178b0dfa0872aff5205fcb273795305ff939ca30a39da7cbc592e544389898@ec2-54-167-152-185.compute-1.amazonaws.com:5432/dc5g3h5n0ps3nl'
-DATABASES['default'] = dj_database_url.parse(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 
