e7ed3d9139419072b24aa39d85a19e70c27fb35e
Horacio Iseka <horacio.chiarella@gmail.com>
2021-07-03 17:16:20 -0300

    probando channels

---
+++ fire-app-cba/alarms.py
@@ -0,0 +1,15 @@
+# from channels.generic.websocket import AsyncWebSocketConsumer
+# 
+# 
+# class NewActiveFire(AsyncWebSocketConsumer):
+# 
+#     async def connect(self):
+# 
+#         await self.accept()
+# 
+#     async def disconnect(self, close_code):
+#         pass
+# 
+#     async def receive(self, fire):
+#         pass
+# 
---
+++ fire-app-cba/routing.py
@@ -0,0 +1,8 @@
+# from django.urls import path
+#
+# from .alarms import NewActiveFire
+#
+# ws_urlpatterns = [
+#     path('ws/fire_alarm', NewActiveFire.as_asgi())
+# ]
+#
