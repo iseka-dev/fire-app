fe2dfac9037a6b72337d360f67265dcefffb7e56
Horacio Iseka <horacio.chiarella@gmail.com>
2021-07-05 00:08:35 -0300

    ongoing working with channels

--- fire-app-cba/alarms.py
+++ fire-app-cba/alarms.py
@@ -1,15 +1,14 @@
-# from channels.generic.websocket import AsyncWebSocketConsumer
-# 
-# 
-# class NewActiveFire(AsyncWebSocketConsumer):
-# 
-#     async def connect(self):
-# 
-#         await self.accept()
-# 
-#     async def disconnect(self, close_code):
-#         pass
-# 
-#     async def receive(self, fire):
-#         pass
-# 
+from channels.generic.websocket import AsyncWebSocketConsumer
+
+
+class NewActiveFire(AsyncWebSocketConsumer):
+
+    async def connect(self):
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        pass
+
+    async def receive(self, fire):
+        pass
--- fire-app-cba/asgi.py
+++ fire-app-cba/asgi.py
@@ -7,11 +7,9 @@
 # https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 # """
 #
-# import os
-#
-# from django.core.asgi import get_asgi_application
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fire-app-cba.settings')
-#
-# application = get_asgi_application()
-#
+import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fire-app-cba.settings')
+
+application = get_asgi_application()
