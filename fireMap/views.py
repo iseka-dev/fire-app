fc935d09c7ead3907077977887d2f8516d8edcd4
Horacio Iseka <horacio.chiarella@gmail.com>
2021-07-30 09:34:45 -0300

    prov changes

--- fireMap/views.py
+++ fireMap/views.py
@@ -3,6 +3,10 @@ from .models import Incendio
 from django.core.serializers import serialize
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from django.utils import timezone
+from pathlib import Path
+from zipfile import ZipFile
+from django.contrib.gis.gdal import DataSource
 
 
 class FireMap(TemplateView):
@@ -26,3 +30,21 @@ def fires(request):
     fires = Incendio.objects.filter(activo=True)
     fires = serialize('geojson', fires)
     return JsonResponse({'fires': fires})
+
+
+@csrf_exempt
+def load(request):
+    ds = DataSource('/path/to/your/cities.shp')
+
+    mapping_tiempo_severo = {
+        'poly': 'MULTIPOLYGON',
+        'pts': 'pts',
+    }
+    lm2 = LayerMapping(
+        TiempoSevero,
+        tempdir,
+        mapping_tiempo_severo,
+        transform=False
+    )
+    lm2.save(verbose=True, strict=True)
+    return HttpResponse("Informacion de Tiempo Severo procesada")
